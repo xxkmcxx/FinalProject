db6567b90e8143b9f16c27e6c241d7f562cd9ba7
j3ic0b <ricky_ortolaza@live.com>
2014-10-15 19:59:21 -0400

    Cout

--- TextEditor-FinalProject/TextGenerator.cpp
+++ TextEditor-FinalProject/TextGenerator.cpp
@@ -10,5 +10,6 @@ int main()
 	cout << "Salieron" << endl;
 	cout << "Kevincoptero" << endl;
 	cout << "kevin mamamelo\n\n\n\n\n\n\n\n\n\n\n";
+	cout << "Hemos perdido tiempo con cojones" << endl;
 	return (0);
 }
