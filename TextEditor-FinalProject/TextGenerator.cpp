8a2e5c2de32bcc3a7c690da7c4b08d8daa6d9f8b
SebastianRdgz <11sebastian29@gmail.com>
2014-10-15 18:27:33 -0400

    Puse un punto.

--- TextEditor-FinalProject/TextGenerator.cpp
+++ TextEditor-FinalProject/TextGenerator.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "this is a test" << endl;
-	cout << "this is a second test" << endl;
+	cout << "this is a second test." << endl;
 	cout << "ARRANCA" << endl;
 	return (0);
 }
