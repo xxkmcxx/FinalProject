037fca7d014c808bb85b86c214e68f8e2b800918
xxkmcxx <kmartinez0890@gmail.com>
2014-10-15 21:28:19 -0400

    Added main definitions of the class FileManager. Also the implementation of the showMenu was added.

--- TextEditor-FinalProject/FileManager.h
+++ TextEditor-FinalProject/FileManager.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include "MyMenu.h"
+#include "EditorManager.h"
+#include <string>
+
+using namespace std;
 class FileManager
 {
+private:
+// Constructors are made private because class methods are static.
+	EditorManager eManager;
+
 public:
 	FileManager();
 	~FileManager();
+	static void openFile();
+	static void createFile();
+	static void copyDocument();
+	static void showMenu();
 };
-
