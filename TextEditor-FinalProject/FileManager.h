efcce3c22c58c8d028ce9932fbc501fc01919266
SebastianRdgz <11sebastian29@gmail.com>
2014-10-15 21:33:31 -0400

    FileManager 1st Sketch

--- TextEditor-FinalProject/FileManager.h
+++ TextEditor-FinalProject/FileManager.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include "ListaDeCadenas.h"
+using namespace std;
+
+const int MAX_FILE = 10;
+const int MAX_SENT = 100;
+
 class FileManager
 {
+private:
+	fstream file;
+	ListaDeCadenas sentence;
+	int quantity;
 public:
 	FileManager();
+	FileManager();
 	~FileManager();
+	void openFile();
+	void closeFile();
+	void newFile();
 };
 
