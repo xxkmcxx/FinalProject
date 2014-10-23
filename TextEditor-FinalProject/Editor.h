7f2796485ba983c22b8084e3afaf4f73f29c16e3
joh-wayne <joshua.valentin1@gmail.com>
2014-10-23 19:50:28 -0400

    Revert "Added Stuff"
    This reverts commit 4db8da15136f7e84989c852f5ad8d7dc7950ad04.

--- TextEditor-FinalProject/Editor.h
+++ TextEditor-FinalProject/Editor.h
@@ -1,23 +1,8 @@
 #pragma once
-#include "FileManager.h"
-
 class Editor
 {
-private:
-	FileManager * Files;
-	int quantityOfFiles;
 public:
-	Editor(const FileManager & a, const FileManager & b, const FileManager & c);
-	Editor(const Editor & aEditor);
+	Editor();
 	~Editor();
-	Editor & operator = (const Editor & aEditor);
-	void EditWord();
-	void EditSentence();
-	void EditParagraph();
-	void EditWhole();
-	void FindEOFSymbol();
-	void FindEnterSymbol();
-	void FindListSymbol();
-
 };
 
